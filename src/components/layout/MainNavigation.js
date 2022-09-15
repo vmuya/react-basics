13b5b089605f1c126712df1814cdd74b1d6a4269
vmuya <victor@archis.company>
2022-09-15 21:54:34 +0300

    render favorites on favorites page

--- src/components/layout/MainNavigation.js
+++ src/components/layout/MainNavigation.js
@@ -1,8 +1,12 @@
+import { useContext } from 'react';
 import { Link } from 'react-router-dom';
 
 import classes from './MainNavigation.module.css';
+import FavoritesContext from '../../store/favorites-context';
 
 const MainNavigation = () => {
+  const favoritesCtxObj = useContext(FavoritesContext);
+
   return (
     <header className={classes.header}>
       <div className={classes.logo}>React Meetups</div>
@@ -15,7 +19,12 @@ const MainNavigation = () => {
             <Link to="/new-meetup">New Meetup</Link>
           </li>
           <li>
-            <Link to="/favorites">My Favorites</Link>
+            <Link to="/favorites">
+              My Favorites{' '}
+              <span className={classes.badge}>
+                {favoritesCtxObj.totalFavorites}
+              </span>
+            </Link>
           </li>
         </ul>
       </nav>
