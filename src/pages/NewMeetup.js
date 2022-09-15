5c111b2bc74ce45d5114c3e958ba9aa49127bdf9
vmuya <victor@archis.company>
2022-09-15 14:19:43 +0300

    send input data to server using http

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,10 +1,23 @@
 import NewMeetupForm from '../components/meetups/NewMeetupForm';
 
 const NewMeetupPage = () => {
+  const addMeetupHandler = (meetupData) => {
+    fetch(
+      'https://react-basics-vmuya-default-rtdb.firebaseio.com/meetups.json',
+      {
+        method: 'POST',
+        body: JSON.stringify(meetupData),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      }
+    );
+  };
+
   return (
     <section>
       <h1>Add New meetup</h1>
-      <NewMeetupForm />
+      <NewMeetupForm onAddMeetup={addMeetupHandler} />
     </section>
   );
 };
